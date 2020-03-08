30f7832c83b36bb4d31c0efab138e4a3c4cd58b7
bakuniverse <30513244+bakuniverse@users.noreply.github.com>
2020-03-08 21:29:54 +0800

    Update shadowsocks-libev-debian.sh

--- shadowsocks-libev-debian.sh
+++ shadowsocks-libev-debian.sh
@@ -12,11 +12,11 @@ export PATH
 # Current folder
 cur_dir=`pwd`
 
-libsodium_file="libsodium-1.0.18"
-libsodium_url="https://github.com/jedisct1/libsodium/releases/download/1.0.18/libsodium-1.0.18.tar.gz"
+libsodium_file="libsodium-stable"
+libsodium_url="https://download.libsodium.org/libsodium/releases/LATEST.tar.gz"
 
-mbedtls_file="mbedtls-2.16.2"
-mbedtls_url="https://tls.mbed.org/download/mbedtls-2.16.2-gpl.tgz"
+mbedtls_file="mbedtls-2.16.4"
+mbedtls_url="https://tls.mbed.org/download/mbedtls-2.16.4-gpl.tgz"
 
 # Stream Ciphers
 ciphers=(
@@ -87,7 +87,7 @@ get_latest_version(){
     [ -z ${ver} ] && echo "Error: Get shadowsocks-libev latest version failed" && exit 1
     shadowsocks_libev_ver="shadowsocks-libev-$(echo ${ver} | sed -e 's/^[a-zA-Z]//g')"
     download_link="https://github.com/shadowsocks/shadowsocks-libev/releases/download/${ver}/${shadowsocks_libev_ver}.tar.gz"
-    init_script_link="https://raw.githubusercontent.com/bakuniverse/shadowsocks_install/master/shadowsocks-libev-debian"
+    init_script_link="https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-libev-debian"
 }
 
 get_opsy(){
